24879af4c15d22f45800ce37066b9f442cbc4765
Mikael Hägerbro <mikaelhagerbro@gmail.com>
2015-11-17 14:51:07 +0100

    FTGP-3841: Support for Fortnox companies

--- Foretagsplatsen.Api2/Entities/Company/CompanyInfo.cs
+++ Foretagsplatsen.Api2/Entities/Company/CompanyInfo.cs
@@ -8,11 +8,19 @@ namespace Foretagsplatsen.Api2.Entities.Company
     public class CompanyInfo
     {
         /// <summary>
+        /// Can be "Company" or "FortnoxCompany", default value if left out is "Company"
+        /// </summary>
+        public string typeOfCompany { get; set; }
+        /// <summary>
         /// Is unique. See the overview documentation for more information.
         /// </summary>
         public string id { get; set; }
         public string name { get; set; }
         public string businessIdentityCode { get; set; }
         public ContactInfo contactInfo { get; set; }
+        /// <summary>
+        /// Fortnox access token. Only used when creating or updating a Fortnox company.
+        /// </summary>
+        public string authorizationCode { get; set; }
     }
 }
