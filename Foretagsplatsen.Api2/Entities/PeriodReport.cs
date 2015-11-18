d31a110cfaed14a31af5bfe97f7745e0d4b26f37
Mikael Hägerbro <mikaelhagerbro@gmail.com>
2015-11-18 11:37:40 +0100

    Adds periodReport Entity

---
+++ Foretagsplatsen.Api2/Entities/PeriodReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// ReSharper disable InconsistentNaming
+
+namespace Foretagsplatsen.Api2.Entities
+{
+    /// <summary>
+    /// Represents a period report.
+    /// </summary>
+    public class PeriodReport
+    {
+
+        public string id { get; set; }
+
+        public string name { get; set; }
+
+        public DateTime created { get; set; }
+
+        public Period period { get; set; }  
+
+    }
+}
